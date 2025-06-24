ca8b80830f878d3820a84456af0f6f226a4d7562
Eliane Maria <146597452+ElianeOSilva@users.noreply.github.com>
2025-06-24 12:23:00 -0300

    Add files via upload

---
+++ Beecrownd 1001.cpp
@@ -0,0 +1,20 @@
+//Beecrowd 1001 Extremamente Básico
+
+
+#include <stdio.h>
+ 
+int main() {
+    int a, b, result;
+    
+    //entrada de dados
+    scanf("%d", &a);
+    scanf("%d", &b);
+    
+    //processamento
+    result = a + b;
+    
+    //saída de dados
+    printf("X = %d\n", result); 
+    
+    return 0;
+}
