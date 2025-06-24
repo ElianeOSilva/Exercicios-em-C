3fccf029695ac780a4340a26bd9691c5ba9161a2
Eliane Maria <146597452+ElianeOSilva@users.noreply.github.com>
2025-06-24 12:53:14 -0300

    Add files via upload

---
+++ Beecrownd 1004.c
@@ -0,0 +1,16 @@
+//Beecrowd 1004 Produto Simples _ Eliane Maria
+
+#include <stdio.h>
+ 
+int main() {
+ 	int v1, v2, resultado;
+ 	
+ 	scanf("%d %d", &v1, &v2);
+	 
+	resultado = v1 * v2;
+ 	
+ 	printf("PROD = %d\n", resultado);
+ 
+    return 0;
+}
+
