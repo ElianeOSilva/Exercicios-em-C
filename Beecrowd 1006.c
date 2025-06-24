7516db7ea83e3eba59181b13a40eb0c5f560b241
Eliane Maria <146597452+ElianeOSilva@users.noreply.github.com>
2025-06-24 14:18:11 -0300

    Add files via upload

---
+++ Beecrowd 1006.c
@@ -0,0 +1,16 @@
+//Beecrowd 1006 Média 2 _ Eliane Maria
+
+#include <stdio.h>
+ 
+int main() {
+	float A, B, C, media;
+	
+	scanf("%f %f %f", &A, &B, &C);
+	 
+	media = (A*2+B*3+C*5)/10;
+    
+    printf("MEDIA = %.1f\n", media);
+     
+    return 0;
+}
+
