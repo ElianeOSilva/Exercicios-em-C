8cf9334d5640a5b3240de88726e3f536144c7435
Eliane Maria <146597452+ElianeOSilva@users.noreply.github.com>
2025-06-24 13:17:56 -0300

    Add files via upload

---
+++ Beecrowd 1007.c
@@ -0,0 +1,16 @@
+//Beecrowd 1007 Diferença _ Eliane Maria
+
+#include <stdio.h>
+ 
+int main() {
+	int A, B, C, D, diferenca;
+	
+	scanf("%d %d %d %d", &A, &B, &C, &D);
+	 
+	diferenca = (A*B-C*D);
+    
+    printf("DIFERENCA = %d\n", diferenca);
+     
+    return 0;
+}
+
